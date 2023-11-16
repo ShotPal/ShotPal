c6b6e43233ab2fda5c2d75e3b761f113ff47ac97
Pinzhe Liu <pliu227@wisc.edu>
2023-11-16 13:36:36 -0600

    add activity main page

--- app/src/main/java/com/cs407/shotpal/MainActivity.java
+++ app/src/main/java/com/cs407/shotpal/MainActivity.java
@@ -2,7 +2,6 @@ package com.cs407.shotpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.wifi.aware.PublishConfig;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
