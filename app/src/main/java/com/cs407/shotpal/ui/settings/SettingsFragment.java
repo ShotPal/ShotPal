428ec4f2bf05b6fe4be2818351136768fc138eea
Xingkuan Yu <xingkuanyu@outlook.com>
2023-12-10 17:20:47 -0600

    fixed button toggles in settings; implemented reset; TODO: IMU calibration

--- app/src/main/java/com/cs407/shotpal/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/cs407/shotpal/ui/settings/SettingsFragment.java
@@ -1,16 +1,17 @@
 package com.cs407.shotpal.ui.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.cs407.shotpal.R;
 import com.cs407.shotpal.databinding.FragmentSettingsBinding;
@@ -19,33 +20,28 @@ public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
 
-    private String sensLevel = "low";
+    private SharedPreferences sharedPreferences;
+    String sensOpt, trackOpt, sigOpt;
 
-    private String trackingOption = "mic";
-    private String signalOption = "sound";
+    static final String DEFAULT_SENS_OPT = "medium";
+    static final String DEFAULT_TRACK_OPT = "dual";
+    static final String DEFAULT_SIG_OPT = "dual";
 
-    ToggleButton lowButton, midButton, highButton;
+    ToggleButton sensLowButton, sensMidButton, sensHighButton;
     ToggleButton micButton, imuButton, dualTrackingButton;
     ToggleButton soundButton, vibrationButton, dualSignalButton;
+    Button calibrationButton, resetButton;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        lowButton = view.findViewById(R.id.sensLowButton);
-        midButton = view.findViewById(R.id.sensMidButton);
-        highButton = view.findViewById(R.id.sensHighButton);
+        sensLowButton = view.findViewById(R.id.sensLowButton);
+        sensMidButton = view.findViewById(R.id.sensMidButton);
+        sensHighButton = view.findViewById(R.id.sensHighButton);
 
         micButton = view.findViewById(R.id.micButton);
         imuButton = view.findViewById(R.id.imuButton);
@@ -55,93 +51,92 @@ public class SettingsFragment extends Fragment {
         vibrationButton = view.findViewById(R.id.vibrationButton);
         dualSignalButton = view.findViewById(R.id.dualSignalButton);
 
-        updateButtonSelection();
+        calibrationButton = view.findViewById(R.id.calibrationButton);
+        resetButton = view.findViewById(R.id.resetButton);
 
-        lowButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                sensLevel = "low";
-                updateButtonSelection();
-        });
-        midButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                sensLevel = "mid";
-                updateButtonSelection();
-        });
+        // Load settings from shared preferences
+        sharedPreferences = requireActivity().getSharedPreferences("settings", 0);
+        sensOpt = sharedPreferences.getString("sensOpt", DEFAULT_SENS_OPT);
+        trackOpt = sharedPreferences.getString("trackOpt", DEFAULT_TRACK_OPT);
+        sigOpt = sharedPreferences.getString("signOpt", DEFAULT_SIG_OPT);
+        Log.println(Log.INFO, "Settings", "Loaded settings: " + sensOpt + " " + trackOpt + " " + sigOpt);
 
-        highButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                sensLevel = "high";
-                updateButtonSelection();
-        });
+        updateButton();
 
-        micButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                trackingOption = "mic";
-                updateButtonSelection();
+        sensLowButton.setOnClickListener(v -> {
+            sensOpt = "low";
+            updateButton();
         });
-
-        imuButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                trackingOption = "imu";
-                updateButtonSelection();
+        sensMidButton.setOnClickListener(v -> {
+            sensOpt = "medium";
+            updateButton();
         });
-
-        dualTrackingButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                trackingOption = "dual";
-                updateButtonSelection();
+        sensHighButton.setOnClickListener(v -> {
+            sensOpt = "high";
+            updateButton();
         });
 
-        soundButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                signalOption = "sound";
-                updateButtonSelection();
+        micButton.setOnClickListener(v -> {
+            trackOpt = "mic";
+            updateButton();
         });
-        soundButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                signalOption = "vibration";
-                updateButtonSelection();
+        imuButton.setOnClickListener(v -> {
+            trackOpt = "imu";
+            updateButton();
         });
-        dualTrackingButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                signalOption = "dual";
-                updateButtonSelection();
+        dualTrackingButton.setOnClickListener(v -> {
+            trackOpt = "dual";
+            updateButton();
         });
+
+        soundButton.setOnClickListener(v -> {
+            sigOpt = "sound";
+            updateButton();
+        });
+        vibrationButton.setOnClickListener(v -> {
+            sigOpt = "vibration";
+            updateButton();
+        });
+        dualSignalButton.setOnClickListener(v -> {
+            sigOpt = "dual";
+            updateButton();
+        });
+
+        calibrationButton.setOnClickListener(v -> calibrationButton());
+        resetButton.setOnClickListener(v -> resetButton());
     }
 
-    private void updateButtonSelection() {
-        if (sensLevel.equals("low")) {
-            lowButton.setChecked(true);
-            midButton.setChecked(false);
-            highButton.setChecked(false);
-        } else if (sensLevel.equals("mid")) {
-            lowButton.setChecked(false);
-            midButton.setChecked(true);
-            highButton.setChecked(false);
-        } else {
-            lowButton.setChecked(false);
-            midButton.setChecked(false);
-            highButton.setChecked(true);
-        }
+    private void updateButton() {
+        sharedPreferences = requireActivity().getSharedPreferences("settings", 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("sensOpt", sensOpt);
+        editor.putString("trackOpt", trackOpt);
+        editor.putString("signOpt", sigOpt);
+        editor.apply();
+        Log.println(Log.INFO, "Settings", "Saved settings: " + sensOpt + " " + trackOpt + " " + sigOpt);
 
-        if (trackingOption.equals("mic")) {
-            micButton.setChecked(true);
-            imuButton.setChecked(false);
-            dualTrackingButton.setChecked(false);
-        } else if (sensLevel.equals("imu")) {
-            micButton.setChecked(false);
-            imuButton.setChecked(true);
-            dualTrackingButton.setChecked(false);
-        } else {
-            micButton.setChecked(false);
-            imuButton.setChecked(false);
-            dualTrackingButton.setChecked(true);
-        }
+        sensLowButton.setChecked(sensOpt.equals("low"));
+        sensMidButton.setChecked(sensOpt.equals("medium"));
+        sensHighButton.setChecked(sensOpt.equals("high"));
 
-        if (signalOption.equals("sound")) {
-            soundButton.setChecked(true);
-            vibrationButton.setChecked(false);
-            dualSignalButton.setChecked(false);
-        } else if (sensLevel.equals("vibration")) {
-            soundButton.setChecked(false);
-            vibrationButton.setChecked(true);
-            dualSignalButton.setChecked(false);
-        } else {
-            soundButton.setChecked(false);
-            vibrationButton.setChecked(false);
-            dualSignalButton.setChecked(true);
-        }
+        micButton.setChecked(trackOpt.equals("mic"));
+        imuButton.setChecked(trackOpt.equals("imu"));
+        dualTrackingButton.setChecked(trackOpt.equals("dual"));
+
+        soundButton.setChecked(sigOpt.equals("sound"));
+        vibrationButton.setChecked(sigOpt.equals("vibration"));
+        dualSignalButton.setChecked(sigOpt.equals("dual"));
+    }
+
+    private void resetButton() {
+        sensOpt = DEFAULT_SENS_OPT;
+        trackOpt = DEFAULT_TRACK_OPT;
+        sigOpt = DEFAULT_SIG_OPT;
+        updateButton();
+    }
+
+    private void calibrationButton() {
+        // TODO: Implement calibration
     }
 
     @Override
